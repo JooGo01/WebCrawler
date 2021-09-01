2a0b84c2d9a30bf03ea1db2c152a93bc4d037811
JooGo01 <75143377+JooGo01@users.noreply.github.com>
2021-09-01 16:37:16 -0300

    Add files via upload

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/JooGo01/WebCrawler.git
+
+go 1.17
+
+require (
+	github.com/steelx/extractlinks v0.0.2 // indirect
+	golang.org/x/net v0.0.0-20191112182307-2180aed22343 // indirect
+)
--- index.go
+++ index.go
@@ -1,7 +1,43 @@
 package main
 
-import "fmt"
+import (
+	"crypto/tls"
+	"fmt"
+	"net/http"
+	"os"
+
+	"github.com/steelx/extractlinks"
+)
 
 func main() {
-	fmt.Println("hello world")
+	URL := "https://www.youtube.com/watch?v=2wmkHFTaXfA&t=767s"
+
+	config := &tls.Config{
+		InsecureSkipVerify: true,
+	}
+
+	transport := &http.Transport{
+		TLSClientConfig: config,
+	}
+
+	netClient := &http.Client{
+		Transport: transport,
+	}
+
+	response, err := netClient.Get(URL)
+	checkError(err)
+	defer response.Body.Close()
+
+	links, err := extractlinks.All(response.Body)
+
+	fmt.Println(links)
+
+}
+
+func checkError(err error) {
+
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 }
